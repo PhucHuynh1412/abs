9d0564bebcef9e6fcdd0c45b23415f208c00c11c
hltphucCompaq <hltphucCompaq@gmail.com>
2022-01-19 00:36:29 +0700

    update setdefault.py add line message2

--- setdefault.py
+++ setdefault.py
@@ -1,15 +1,19 @@
 import pprint
 
 message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+message2 = 'phuclovenhu'
 
 count_chars = {}
+count_chars2 = {}
 
 for char in message:
     count_chars.setdefault(char,0)
     count_chars[char] += 1
 
-print(count_chars)
-
-print('-'*20)
-
 pprint.pprint(count_chars)
+
+for char in message2:
+	count_chars2.setdefault(char, 0)
+	count_chars2[char] += 1
+
+pprint.pprint(count_chars2)
